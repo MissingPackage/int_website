521a2a619431f3caaf301894ecda97fb46f7f326
msieurmoustache <fbrobillard@gmail.com>
2024-08-26 10:17:13 -0400

    clean unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 // src/App.tsx
-import React, { useState } from 'react';
+import React from 'react';
 import HomePage from './components/HomePage.jsx';
 import AboutPage from './components/AboutPage.jsx';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/Layout.js';
 
 const App = () => {
-  const [page, setPage] = useState('home');
-
-  const renderPage = () => {
-    switch (page) {
-      case 'home':
-        return <HomePage onNavigate={setPage} />;
-      case 'about':
-        return <AboutPage onNavigate={setPage} />;
-      default:
-        return <HomePage onNavigate={setPage} />;
-    }
-  };
-
   return (
     <div>
       <Routes>
